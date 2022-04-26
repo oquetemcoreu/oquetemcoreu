296d9e11e11a7bc2611047dad22abb5fd79902f4
Shermam Miranda <shermam.miranda@goodeggs.com>
2022-04-26 10:16:32 -0300

    add js file

---
+++ public/index.js
@@ -0,0 +1,10 @@
+// There is good course here
+// https://firebase.google.com/codelabs/firestore-web#0
+
+// https://firebase.google.com/docs/firestore/use-rest-api?authuser=2
+
+const createUserButton = document.querySelector("#createUser");
+
+createUserButton.addEventListener("click", async (e) => {
+  alert("Hellow world");
+});
